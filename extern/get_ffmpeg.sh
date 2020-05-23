3f444447e3a6ab3c63833f93c8462a1530358135
Frank Huang <me@nongraphical.com>
2020-05-22 17:03:41 -0700

    Add misc stuff

---
+++ extern/get_ffmpeg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget --continue "https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-amd64-static.tar.xz"
+tar xvf ffmpeg-git-amd64-static.tar.xz
+mv ffmpeg-git-20200504-amd64-static ffmpeg
