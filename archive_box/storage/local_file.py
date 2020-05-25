9528bf7eee26851c5a8b68697efcf520e06a1d81
Frank Huang <me@fyhuang.com>
2020-05-24 22:43:12 -0700

    Upload/download byte ranges

--- archive_box/storage/local_file.py
+++ archive_box/storage/local_file.py
@@ -1,12 +1,56 @@
 import os
+import io
 import shutil
+import functools
 from pathlib import Path
+from typing import Optional, Tuple, Any
+from typing_extensions import Protocol
 
 from .stored_data import StoredDataId
 
+
+def _common_ancestor(p1: Path, p2: Path) -> Path:
+    min_len = min(len(p1.parts), len(p2.parts))
+    result = []
+    for i in range(min_len):
+        if p1.parts[i] != p2.parts[i]:
+            break
+        result.append(Path(p1.parts[i]))
+    return functools.reduce(lambda a, b: a / b, result)
+
+
+class Reader(Protocol):
+    def __enter__(self) -> 'Reader':
+        pass
+    def __exit__(self, exc_type, exc_value, traceback):
+        pass
+    def read(self, size = -1) -> bytes:
+        pass
+
+
+class ByteRangeReader(object):
+    def __init__(self, wrapped_stream, max_bytes):
+        self.wrapped_stream = wrapped_stream
+        self.max_bytes = max_bytes
+        self.read_so_far = 0
+
+    def __enter__(self) -> Reader:
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        return self.wrapped_stream.__exit__(exc_type, exc_value, traceback)
+
+    def read(self, wanted_size = -1):
+        remaining = self.max_bytes - self.read_so_far
+        read_size = min(remaining, wanted_size)
+        result = self.wrapped_stream.read(read_size)
+        self.read_so_far += len(result)
+        return result
+
+
 class LocalFileStorage(object):
     def __init__(self, root_dir: Path):
-        self.root = root_dir
+        self.root = root_dir.resolve()
         self.tempdir = root_dir / "temp"
         os.makedirs(self.tempdir, exist_ok=True)
 
@@ -14,19 +58,49 @@ class LocalFileStorage(object):
         first_part = sdid.schema + sdid.id[:2]
         return self.root / first_part / sdid.id
 
+    def _is_path_in_root(self, path: Path) -> bool:
+        path_abs = path.resolve()
+        ancestor = _common_ancestor(self.root, path_abs)
+        return ancestor == self.root
+
     def contains(self, sdid: StoredDataId) -> bool:
         path = self._to_path(sdid)
         return path.exists()
 
-    def store(self, src_file, sdid) -> None:
+    def upload(self, sdid: StoredDataId, src_file: Path) -> None:
         path = self._to_path(sdid)
         if path.exists():
             return
+        os.makedirs(path.parent, exist_ok=True)
 
-        # In case src_file and root are on different filesystems,
-        # copy to a temp file first and then atomically move
-        temp_path = self.tempdir / sdid.to_strid() + ".tmp"
+        # Copy to a temp file first and then atomically move
+        temp_path = self.tempdir / (sdid.to_strid() + ".tmp")
         shutil.copy2(src_file, temp_path)
         os.rename(temp_path, path)
 
+    def move_inplace(self, sdid: StoredDataId, src_file: Path) -> None:
+        # Move a pre-uploaded file into the right place
+        # This is an internal function that allows LocalFileStorage to act like
+        # a "zero-copy" cache
+        if not self._is_path_in_root(src_file):
+            raise RuntimeError("File should already be in the root to use move_inplace")
 
+        path = self._to_path(sdid)
+        if path.exists():
+            return
+        os.rename(src_file, path)
+
+    def download_stat(self, sdid: StoredDataId) -> os.stat_result:
+        pass
+
+    def download_to(self, sdid: StoredDataId, dest_filename: Path) -> None:
+        pass
+
+    def download_bytes(self, sdid: StoredDataId, byte_range: Optional[Tuple[int, int]] = None) -> Reader:
+        path = self._to_path(sdid)
+        f = path.open("rb")
+        if byte_range is not None:
+            f.seek(byte_range[0])
+            return ByteRangeReader(f, byte_range[1] - byte_range[0])
+        else:
+            return f
